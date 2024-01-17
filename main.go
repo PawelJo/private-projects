b90b26c1524e471b274fe2871168ea3086e4a430
PawelJo <57221666+PawelJo@users.noreply.github.com>
2024-01-17 17:49:32 +0100

    Return user via API and assign username to greeting

--- main.go
+++ main.go
@@ -2,13 +2,50 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
-	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Rating int    `json:"rating"`
+}
+
+func writeUser() {
+	data := User{
+		Id:     6,
+		Name:   "Hurensohn",
+		Rating: 4,
+	}
+
+	file, _ := json.MarshalIndent(data, "", "")
+
+	_ = ioutil.WriteFile("test.json", file, 0644)
+}
+
+func getUsers() (users []User) {
+
+	fileBytes, err := ioutil.ReadFile("data.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(fileBytes, &users)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return users
+
+}
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
@@ -17,32 +54,19 @@ func enableCors(w *http.ResponseWriter) {
 func get(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
 
-	data, err := os.ReadFile("data.json")
-	if err != nil {
-		fmt.Println("An Error Occured:", err)
-		return
-	}
-	fmt.Println("Your data is:", data)
-	// Parse JSON
-	var jsonData []map[string]interface{}
-	if err := json.Unmarshal(data, &jsonData); err != nil {
-		http.Error(w, "Error parsing JSON data", http.StatusInternalServerError)
-		return
-	}
-
-	jsonResponse, err := json.Marshal(jsonData)
-	if err != nil {
-		http.Error(w, "Error converting Data to JSON", http.StatusInternalServerError)
-		return
-	}
-
-	/* defer jsonFile.Close() */
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	/* w.Write([]byte(`{"message": "get called"}`)) */
+	users := getUsers()
 
-	w.Write(jsonResponse)
+	rand.Seed(time.Now().UnixNano())     // seed or it will be set to 1
+	randomIndex := rand.Intn(len(users)) // generate a random int in the range 0 to 9
+
+	user, err := json.Marshal(users[randomIndex])
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write(user)
 }
 
 func post(w http.ResponseWriter, r *http.Request) {
@@ -75,58 +99,16 @@ func notFound(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "not found"}`))
 }
 
+/*
 type Users struct {
 	Users []User `json:"users"`
-}
+} */
 
-type User struct {
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Age    int    `json:"Age"`
-	Social Social `json:"social"`
-}
-
-type Social struct {
-	Facebook string `json:"facebook"`
-	Twitter  string `json:"twitter"`
-}
-
-/*
-	func readJson() {
-		jsonFile, err := os.Open("data.json")
-
-		if err != nil {
-			fmt.Println(err)
-		}
-		var users Users
-
-byteValue, _ := io.ReadAll(jsonFile)
-
-		fmt.Println(len(users.Users))
-		json.Unmarshal(byteValue, &users)
-		 for i := 0; i < len(users.Users); i++ {
-			fmt.Println("User Type: " + users.Users[i].Type)
-			fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
-			fmt.Println("User Name: " + users.Users[i].Name)
-			fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
-			fmt.Println("\n")
-		}
-		fmt.Println(users)
-		fmt.Println("User Type: " + users.Users[2].Type)
-		fmt.Println("User Age: " + strconv.Itoa(users.Users[2].Age))
-		fmt.Println("User Name: " + users.Users[2].Name)
-		fmt.Println("Facebook Url: " + users.Users[2].Social.Facebook)
-		fmt.Println("\n")
-
-		byteArray, err := io.ReadAll(jsonFile)
-		json.Marshal(byteArray)
-		fmt.Println(string(byteArray))
-
-		defer jsonFile.Close()
-	}
-*/
 func main() {
 
+	writeUser()
+
+	getUsers()
 	/*  	 */
 
 	r := mux.NewRouter()
@@ -138,5 +120,3 @@ func main() {
 	r.HandleFunc("/", notFound)
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
-
-// TODO: Figure out how the fuck to return the mapped users from json
